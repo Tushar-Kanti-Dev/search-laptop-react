b25daef81c901a21598845fbbe434125a3b4e6ff
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-03-04 01:21:33 +0600

    another way search added

--- src/components/Laptops/Laptops.js
+++ src/components/Laptops/Laptops.js
@@ -1,20 +1,38 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useLaptops from '../../hooks/useLaptops';
 import LaptopCart from '../LaptopCart/LaptopCart';
 import './Laptops.css'
 
 const Laptops = () => {
-    const [laptops, setLaptops] = useLaptops();
+    // const [laptops, setLaptops] = useLaptops();
+    const [searchText, setSearchText] = useState('');
     const [searchResult, setSearchResult] = useState([]);
+
+    useEffect( ()=>{
+        console.log('check to understand useffect');
+        fetch('laptopData.json')
+        .then(res => res.json())
+        .then( data =>{
+            const match = data.filter(d => d.model.includes(searchText));
+            setSearchResult(match);
+        });
+
+
+    }, [searchText])
+
     const searchLaptop = event =>{
-        const searchText = event.target.value;
-        const match = laptops.filter(l => l.model.includes(searchText));
-        setSearchResult(match);
+        setSearchText(event.target.value);
+        
     }
+    // const searchLaptop = event =>{
+    //     const searchText = event.target.value;
+    //     const match = laptops.filter(l => l.model.includes(searchText));
+    //     setSearchResult(match);
+    // }
     return (
         <div>
-            <h4>Laptops components......{laptops.length}</h4>
+            <h4>Laptops components......{searchResult.length}</h4>
             <div className="search-field">
                 <input onChange={searchLaptop} type="text" placeholder='Search' />
             </div>
